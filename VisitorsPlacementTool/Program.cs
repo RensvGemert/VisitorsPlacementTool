93386b33cbaaf6967129fff1314876d56cf5edbd
Rens <rensvgemert@gmail.com>
2021-10-09 19:13:20 +0200

    Add project files.

---
+++ VisitorsPlacementTool/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VisitorsPlacementTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Visitor visitor = new Visitor("Rens", 11);
+
+            visitor.CheckIfAdult(visitor);
+        }
+    }
+}
---
+++ VisitorsPlacementTool/Visitor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorsPlacementTool
+{
+    public class Visitor
+    {
+        public string Name;
+        public int Age;
+        public bool Adult;
+        public Visitor(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        // Age check
+        public void CheckIfAdult(Visitor bezoeker)
+        {
+            if(Age > 12)
+            {
+                Console.WriteLine(bezoeker.Name + " is an adult");
+                bezoeker.Adult = true;
+            }
+            else
+            {
+                Console.WriteLine(bezoeker.Name + " is not adult");
+                bezoeker.Adult = false;
+            }
+        }
+
+    }
+}
