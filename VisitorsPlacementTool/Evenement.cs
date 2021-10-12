2bacdefe4b1120b1f2f844344a78d2d2a2f4b38e
i461328 <i461328@fhict.nl>
2021-10-12 12:56:36 +0200

    Evenement class created

---
+++ VisitorsPlacementTool/Evenement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorsPlacementTool
+{
+    public class Evenement
+    {    
+        public string EvenementNaam; // Ronde van Eindhoven
+        public DateTime Datum; // Datum van het evenement
+        public int AantalVakken; // Aantal vakken beschikbaar
+        public int AantalStoelen; // Aantal stoelen beschikbaar (is ook max aantal bezoekers)
+        public Evenement(string EvenementNaam, DateTime Datum, int AantalVakken, int AantalStoelen)
+        {
+            this.EvenementNaam = EvenementNaam;
+            this.Datum = Datum;
+            this.AantalVakken = AantalVakken;
+            this.AantalStoelen = AantalStoelen;
+        }
+    }
+}
--- VisitorsPlacementTool/Program.cs
+++ VisitorsPlacementTool/Program.cs
@@ -8,10 +8,12 @@ namespace VisitorsPlacementTool
     {
         static void Main(string[] args)
         {
-            Visitor visitor = new Visitor("Rens", 23);
-            Visitor visitor2 = new Visitor("Klaas", 11);
-            visitor.CheckIfAdult(visitor);
-            visitor2.CheckIfAdult(visitor2);
+            Evenement evenement = new Evenement("Ronde van Eindhoven", new DateTime(2021, 10, 12), 4, 5);
+
+            Console.WriteLine("Evenement: " + evenement.EvenementNaam);
+            Console.WriteLine("Datum: " + evenement.Datum.ToShortDateString());
+            Console.WriteLine("Aantal vakken: " + evenement.AantalVakken);
+            Console.WriteLine("Aantal stoelen: " + evenement.AantalStoelen);
         }
     }
 }
