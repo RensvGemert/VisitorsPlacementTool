b81c9532ce2b372c89361134e4eef29fa5344d5b
RensvGemert <84020568+RensvGemert@users.noreply.github.com>
2021-12-03 13:55:48 +0100

    FilterOpTijdAangemeldeBezoekersTest Unit Test added

---
+++ VisitorsPlacementToolTests/Evenement_FilterOpTijdAangemeldeBezoekers_Tests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Tests
+{
+    [TestClass()]
+    public class Evenement_FilterOpTijdAangemeldeBezoekers_Tests
+    {
+        [TestMethod()]
+        public void FilterOpTijdAangemeldeBezoekersTest()
+        {
+            Organisatie organisatie = new Organisatie("Organisatie");
+            List<Vak> vakken = organisatie.MaakVakken();
+            Evenement evenement = new Evenement("Parcour Eindhoven", vakken, DateTime.Today);
+
+            List<Bezoeker> TestBezoekers = new List<Bezoeker>();
+            TestBezoekers.Add(new Bezoeker("Kind1", new DateTime(2014, 2, 2), DateTime.Today.AddDays(-2)));
+            TestBezoekers.Add(new Bezoeker("Kind2", new DateTime(2014, 2, 2), DateTime.Today.AddDays(-2)));
+            TestBezoekers.Add(new Bezoeker("Kind3", new DateTime(2014, 2, 2), DateTime.Today.AddDays(1)));
+
+            int expectedResult = 2;
+            int result = evenement.FilterOpTijdAangemeldeBezoekers(TestBezoekers).Count;
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
