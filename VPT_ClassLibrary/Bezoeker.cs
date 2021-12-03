202ad8dd2c3edd58b01b5a23d9992e5a238713dd
RensvGemert <84020568+RensvGemert@users.noreply.github.com>
2021-12-03 10:19:52 +0100

    Update geboortedatum

--- VPT_ClassLibrary/Bezoeker.cs
+++ VPT_ClassLibrary/Bezoeker.cs
@@ -9,16 +9,16 @@ namespace Domain
     public class Bezoeker
     {
         public string Naam { get; set; }
-        public int Leeftijd { get; set; }
+        public DateTime GeboorteDatum { get; set; }
 
         public bool IsGeplaatst = false;
-
+        public int Leeftijd { get; set; }
         private readonly int _minimumLeeftijd = 12;
 
-        public Bezoeker(string naam, int leeftijd)
+        public Bezoeker(string naam, DateTime geboorteDatum)
         {
             Naam = naam;
-            Leeftijd = leeftijd;
+            GeboorteDatum = geboorteDatum;
             IsGeplaatst = false;
         }
 
@@ -32,6 +32,16 @@ namespace Domain
             {
                 return false;
             }
+        }
+
+        public int GetAge(DateTime Geboortedatum)
+        {
+            var today = DateTime.Today;
+
+            var a = (today.Year * 100 + today.Month) * 100 + today.Day;
+            var b = (Geboortedatum.Year * 100 + Geboortedatum.Month) * 100 + Geboortedatum.Day;
+
+            return (a - b) / 10000;
         }
     }
 }
